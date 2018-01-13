d46a1266f448fdc20b92d17e683ebe743c76c0ec
Alexander Nilsson <22331806+SeatloN@users.noreply.github.com>
2018-01-13 15:14:32 +0100

    Update of project

--- honorbet.js
+++ honorbet.js
@@ -78,6 +78,10 @@ client.on("chat", function(channel, userstate, message, self) {
   CountryCheck = ["sweden", "norway", "denmark", "finland"];
   LoserScore = ["20","19","18","17","16","15","14","13","12","11","10","9","8","7","6","5","4","3","2","1","0"];
   WinnerScore = ["16","19","22"];
+
+  Match16 = ["14","13","12","11","10","9","8","7","6","5","4","3","2","1","0"];
+  Match19 = ["18","17","16","15"];
+  Match22 = ["20","19","18"];
   /*
   Code for submit and such.
 
@@ -89,30 +93,83 @@ client.on("chat", function(channel, userstate, message, self) {
 
 
   if (WordArray[0] === HonorPrefix) {
-    console.log("We have a matched Msg");
+    //console.log("We have a matched Msg");
     // Lets bring in the whole array to work our magic!
 
-    country = WordArray[1].toLowerCase(); console.log(country);
-    score = WordArray[2]; console.log(score);
+    country = WordArray[1].toLowerCase(); //console.log(country);
+    score = WordArray[2]; //console.log(score);
     // We are going to start matching everything to just get correct values in score ! 
 
       ToMatchScore = score.split("-");
       if (WinnerScore.indexOf(ToMatchScore[0]) >= 0) {
         //client.say("kingofnordic", "Winner: "+ ToMatchScore[0]);
-        console.log("Winner: "+ ToMatchScore[0]);
+        //console.log("Winner: "+ ToMatchScore[0]);
         WinScore = 1;
       } else {
         WinScore = 0;
       }
       if (LoserScore.indexOf(ToMatchScore[1]) >= 0) {
         //client.say("kingofnordic", "Loser: "+ ToMatchScore[1]);
-        console.log("Loser: "+ ToMatchScore[1]);
+        //console.log("Loser: "+ ToMatchScore[1]);
         LoseScore = 1;
       } else {
         LoseScore = 0;
       }
-      ScoreAfterFilter = ToMatchScore[0] + " - " + ToMatchScore[1];
+
+      if (ToMatchScore[0] === "16") {
+        // Check if loser is in the 
+        if (Match16.indexOf(ToMatchScore[1]) >= 0) {
+          console.log("Loser score is alright for 16: "+ ToMatchScore[1]);
+          Check16 = 1;
+        } else {
+          Check16 = 0;
+        }
+      }
+      if (ToMatchScore[0] === "19") {
+        if (Match19.indexOf(ToMatchScore[1]) >= 0) {
+          console.log("Loser score is alright for 19: "+ ToMatchScore[1]);
+          Check19 = 1;
+        } else {
+          Check19 = 0;
+        }
+      }
+      if (ToMatchScore[0] === "22") {
+        if (Match22.indexOf(ToMatchScore[1]) >= 0) {
+          console.log("Loser score is alright for 22: "+ ToMatchScore[1]);
+          Check22 = 1;
+        } else {
+          Check22 = 0;
+        }
+      }
+      ScoreAfterFilter = ToMatchScore[0] + "-" + ToMatchScore[1];
+      /*
       console.log("WinnerScore: "+ ToMatchScore[0]+" LoserScore: "+ ToMatchScore[1]);
+      console.log(ScoreAfterFilter);
+
+      End of score check 
+      */
+      // Checking that the country is correct. 
+
+
+
+      // Checking that everything is correct for insert into the database.
+
+      if (CountryCheck.indexOf(country) >= 0) {
+        console.log("Country is a match");
+        MatchCountry = 1;
+      } else {
+        MatchCountry = 0;
+      }
+
+      /*
+      End country check feature.
+      */
+
+      /*
+      
+      */
+
+
     }
 
 });
